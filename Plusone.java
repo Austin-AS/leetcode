0b84c55d3be162329fc628d9a388e9c21294fd38
Austin-AS <austinaswin1@gmail.com>
2026-01-08 19:38:21 +0530

    Plusone.java

--- Plusone.java
+++ Plusone.java
@@ -12,3 +12,11 @@ class Solution {
         return result;
     }
 }
+
+
+for(int i=n-1;i>=0;i--){
+    digits[i]=digits[i]+1;
+    digits[i]=digits[i]%10;
+    if(digits[i]!=0)
+        return digits;
+}
