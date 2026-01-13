f845fe90b9caac8b0d5e55242003180c89017db5
Austin-AS <austinaswin1@gmail.com>
2026-01-13 18:39:22 +0530

    LengthofLastWord.java

--- LengthofLastWord.java
+++ LengthofLastWord.java
@@ -1,12 +1,12 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int length=0;
+        int len=0;
         int i=s.length() -1;
         while(i>=0 && s.charAt(i)==' '){
             i--;
         }while(i>=0 && s.charAt(i) !=' '){
-            length++;
+            len++;
             i--;
-        }return length;
+        }return len;
     }
 }
